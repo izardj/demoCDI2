48102db1921d9afbd1c5bb99534c6f24f0b4ba23
izardj <izard.jerome@free.fr>
2017-04-18 12:21:42 +0200

    Implémentation "JDBC"

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -7,16 +7,16 @@ public class Dao implements IDao {
 
 	@Override
 	public String appelDao() {
-		return "Bonjour, je suis bien dans la DAO";
+		return "Bonjour, je suis bien dans la DAO JDBC";
 	}
 
 	@PostConstruct
 	public void loagApresConstruction(){
-		System.out.println("Après création Dao");
+		System.out.println("Après création Dao JDBC");
 	}
 	
 	@PreDestroy
 	public void logAvantDestruction(){
-		System.out.println("Avant destruction Dao");
+		System.out.println("Avant destruction Dao JDBC");
 	}
 }
